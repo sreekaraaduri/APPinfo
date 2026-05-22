22d2a18825267b630dafe7ccd6920ca66a3db294
sreekaraaduri <sreekaraaduri@gmail.com>
2026-05-22 11:53:23 +0530

    till variables

---
+++ Operator.dart
@@ -0,0 +1,23 @@
+void main() {
+  //Operator - Actions
+
+  //1.Arthmetic Operator -> + , - , / , * , %
+
+  int price = 100;
+  int qty = 2;
+
+  int total = price * qty;
+  print("The Total Bill is $total");
+
+  //2.Relational / Comparison Operator
+  var a = 10;
+  var b = 15;
+
+  bool res = a > b;
+
+  print(res);
+
+  double bal = 800.00;
+  bool canPay = bal >= 800;
+  print(canPay);
+}
---
+++ index.dart
@@ -0,0 +1,47 @@
+void main() {
+  print("variable - Store Data");
+
+  // 1. String - "Anything between qutoes is consider as string - Text"
+
+  String email = "sreekaraaduri@gmail.com";
+  print(email);
+
+  // 2. int - keyword - whole number
+
+  int a = 121212;
+  print(a);
+
+  // 3. doudle - keyword - Decimal Number
+
+  double b = 5.9;
+  print(b);
+
+  // 4. bool - keyword - true or false
+  bool dark = true;
+  bool light = false;
+
+  print(dark);
+  print(light);
+
+  // 6. var - Dart figures out by itself
+
+  var name = "Sreekara babu";
+  var age = 25;
+
+  print(name);
+  print(age);
+
+  // 7. dyanmic - Can Change Type
+
+  dynamic data = "Apple";
+  data = 95;
+  data = true;
+
+  print(data);
+
+  // 8. const - Fixed Foever
+
+  const app = "Facebook";
+  
+  print(app);
+}
